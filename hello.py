2a4da4ef9df969897888c7ff5f893f8bb2075a6a
balagesh <balagesh.h@gmail.com>
2024-11-29 16:40:42 +0000

    atnevezett

---
+++ hello.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+print("Na, mizu?")
+x = input("Kérsz fagyit? ")
+if x in ["Igen.", "Ja.", "Persze."]:
+    print("Kérj nekem is")
+else:
+    print("Nem baj, úgysincs fagyi.")
